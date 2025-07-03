299bcddd9e74ccb4e87efb7429ef9f0a9787ee51
suemake <feellikeit2020@naver.com>
2025-07-03 14:50:42 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    const category_first_slide = new Swiper('.category_first_slide', {
+        loop: true,
+        slidesPerView: 1.5,
+        slidesToScroll: 1,
+        spaceBetween: 80,
+        speed: 1000,
+
+    });
+
+
+});
